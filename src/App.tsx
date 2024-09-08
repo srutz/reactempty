083bbf015c9a7ddc0563400abef31e69d0b6a231
stepan.rutz <stepan.rutz@gmx.de>
2024-09-08 15:03:11 +0200

    fetch quotes

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,27 @@
+import { useEffect, useState } from "react";
+
+
+type QuoteType = { id: number; quote: string; author: string }
+
+function Quotes() {
+    const [quotes, setQuotes] = useState<QuoteType[]>([])
+    useEffect(() => {
+        (async () => {
+            const response = await fetch('https://dummyjson.com/quotes')
+            const data = await response.json()
+            setQuotes(data.quotes as QuoteType[])
+        })()
+    }, [])
+    return <ul>
+        {quotes.map((q) => <li key={q.id}>{q.quote} - {q.author}</li>)}
+    </ul>
+}
 
 export function App() {
     return (
-        <div className="text-3xl">Hello</div>
+        <div className="text-3xl">
+            <Quotes></Quotes>
+        </div>
     )
 }
 
