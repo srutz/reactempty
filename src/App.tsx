1ba006f9b91e6872045ee66a6f636739d3319ce4
sr <stepan.rutz@gmx.de>
2024-09-09 16:00:30 +0200

    collapseme, some small components

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,37 @@
-import { useState } from "react"
+import { ReactNode, useState } from "react"
 
+export type ButtonProps =  { clicked: () => void, children: ReactNode }
+
+export function PrimaryButton(props: ButtonProps) {
+    return <button className="pbutton" onClick={props.clicked}>Toggle</button>
+}
+
+export function SecondaryButton(props: ButtonProps) {
+    return <button className="sbutton" onClick={props.clicked}>Toggle</button>
+}
+
+export function Button(props: ButtonProps & { buttontype: "prim" | "sec" }) {
+   return props.buttontype == "prim"
+    ? <PrimaryButton {...props}></PrimaryButton>
+    : <SecondaryButton {...props}></SecondaryButton>
+}
+
+export function ShadowAndBorder(props: { children: ReactNode }) {
+    return <div className="m-4 flex flex-col gap-2 shadow-xl border border-red-300 p-4 rounded-lg">
+        {props.children}
+    </div>
+} 
+
+export function CollapseMe(props: { children: ReactNode}) {
+    const [open,setOpen] = useState(false)
+    return <ShadowAndBorder>
+        {open && props.children}
+        <Button buttontype="prim" clicked={() => setOpen(!open)}>Toggle</Button>
+        
+    </ShadowAndBorder>
+}
 
 export function App() {
-    const [count,setCount] = useState(10)
-    console.log("Render App " + JSON.stringify(count))
-    const clicked = () => { 
-        setCount(count + 1)
-    }
-    return <button onClick={clicked}>Count is {count}</button>
+    return <CollapseMe><h1>hi ehre</h1><div>oiewf</div></CollapseMe>
 }
 
