b5f535bd5bf28a2b8b08b4ee7086a6fad596f1b3
sr <stepan.rutz@gmx.de>
2024-09-10 11:12:01 +0200

    timer with useEffect and cleanup

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,21 @@
+import { useEffect, useState } from "react"
 
 export function App() {
+    const [time, setTime] = useState(new Date().toLocaleTimeString())
+    useEffect(
+        () => {
+            /* begin effect code */
+            const interval = setInterval(() => {
+                setTime(new Date().toLocaleTimeString())
+            }, 1_000)
+            return () => {
+                clearInterval(interval)                
+            }
+            /* end effect code */
+        },
+        [ time ])
     return (
-        <div className="text-3xl">Hello</div>
+        <div className="text-3xl">{time}</div>
     )
 }
 
