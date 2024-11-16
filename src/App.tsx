2c89d01a7ee06f97faa74a37a4b3aed0e7b5d79b
stepan.rutz <stepan.rutz@gmx.de>
2024-11-16 09:03:31 +0100

    vite audit fix

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,123 @@
+import { DrawArea } from "./DrawArea";
+import { DrawItem } from "./DrawItem";
+import { Svg } from "./Svg";
 
 export function App() {
     return (
-        <div className="text-3xl">Hello</div>
+        <DrawArea>
+            <DrawItem position={{ x: 50, y: 100 }}>
+                <Svg title="Green">
+                    <g
+                        transform="matrix(.62947, 0.0000, 0.0000, .62947, -10.072, -9.4421)"
+                        id="g25">
+                        <clipPath
+                            id="clip2">
+                            <path
+                                d="M 16.000 15.000 L 16.000 567.00 L 433.00 567.00 L 433.00 15.000 z"
+                                id="path16" />
+                        </clipPath>
+                        <g
+                            clipPath="url(#clip2)"
+                            id="g23">
+                            <g
+                                fillOpacity=".50196"
+                                fillRule="evenodd"
+                                stroke="none"
+                                fill="#00ff00"
+                                id="g21">
+                                <path
+                                    d="M 21.505 156.88 L 100.64 156.88 L 235.74 21.788 L 21.505 21.788 z"
+                                    id="path19" />
+                            </g>
+                        </g>
+                    </g>
+                </Svg>
+            </DrawItem>
+            <DrawItem position={{ x: 50, y: 200 }}>
+                <Svg title="Yellow">
+                    <g
+                        transform="matrix(.62947, 0.0000, 0.0000, .62947, -10.072, -9.4421)"
+                        id="g36">
+                        <clipPath
+                            id="clip3">
+                            <path
+                                d="M 16.000 15.000 L 16.000 567.00 L 433.00 567.00 L 433.00 15.000 z"
+                                id="path27" />
+                        </clipPath>
+                        <g
+                            clipPath="url(#clip3)"
+                            id="g34">
+                            <g
+                                fillOpacity=".50196"
+                                fillRule="evenodd"
+                                stroke="none"
+                                fill="#ffff00"
+                                id="g32">
+                                <path
+                                    d="M 100.64 156.88 L 156.60 156.88 L 156.60 291.98 L 426.79 21.788 L 235.74 21.788 z"
+                                    id="path30" />
+                            </g>
+                        </g>
+                    </g>
+                </Svg>
+            </DrawItem>
+            <DrawItem position={{ x: 50, y: 300 }}>
+                <Svg title="Pink">
+                    <g
+                        transform="matrix(.62947, 0.0000, 0.0000, .62947, -10.072, -9.4421)"
+                        id="g47">
+                        <clipPath
+                            id="clip4">
+                            <path
+                                d="M 16.000 15.000 L 16.000 567.00 L 433.00 567.00 L 433.00 15.000 z"
+                                id="path38" />
+                        </clipPath>
+                        <g
+                            clipPath="url(#clip4)"
+                            id="g45">
+                            <g
+                                fillOpacity=".50196"
+                                fillRule="evenodd"
+                                stroke="none"
+                                fill="#ff0000"
+                                id="g43">
+                                <path
+                                    d="M 291.70 156.88 L 426.79 156.88 L 426.79 21.788 z"
+                                    id="path41" />
+                            </g>
+                        </g>
+                    </g>
+                </Svg>
+            </DrawItem>
+            <DrawItem position={{ x: 50, y: 400 }}>
+                <Svg title="Blue">
+                    <g
+                        transform="matrix(.62947, 0.0000, 0.0000, .62947, -10.072, -9.4421)"
+                        id="g58">
+                        <clipPath
+                            id="clip5">
+                            <path
+                                d="M 16.000 15.000 L 16.000 567.00 L 433.00 567.00 L 433.00 15.000 z"
+                                id="path49" />
+                        </clipPath>
+                        <g
+                            clipPath="url(#clip5)"
+                            id="g56">
+                            <g
+                                fillOpacity=".50196"
+                                fillRule="evenodd"
+                                stroke="none"
+                                fill="#0000ff"
+                                id="g54">
+                                <path
+                                    d="M 156.60 291.98 L 156.60 562.17 L 291.70 562.17 L 291.70 156.88 z"
+                                    id="path52" />
+                            </g>
+                        </g>
+                    </g>
+                </Svg>
+            </DrawItem>
+        </DrawArea>
     )
 }
 
