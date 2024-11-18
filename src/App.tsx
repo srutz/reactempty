e73225afd9d64fa182ef9ebd96a916a8c76f0268
stepan.rutz <stepan.rutz@gmx.de>
2024-11-18 12:02:18 +0100

    tailwindcss-motion

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,30 @@
+import { ReactNode } from "react"
 
 export function App() {
     return (
-        <div className="text-3xl">Hello</div>
+        <div className="bg-gray-200 w-full h-full flex flex-col gap-4 items-center">
+            <Box>
+                <BigText text="hello"></BigText>
+                <BigText text="again"></BigText>
+                <div>Die Woche schneit es wahrscheinlich</div>
+            </Box>
+            <Box>
+                <BigText text="bonjour"></BigText>
+            </Box>
+        </div>
     )
 }
 
+function BigText(props: { text: string }) {
+    return (
+        <div className="text-4xl">{props.text}</div>
+    )
+}
+
+function Box(props: { children: ReactNode }) {
+    return (
+        <div className="motion-preset-pop bg-white border border-gray-300 shadow-xl rounded-lg m-2 p-4">
+            {props.children}
+        </div>
+    )
+}
