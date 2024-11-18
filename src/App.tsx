6ba304330676bed7450c02e8fb9dd0c14379ca39
stepan.rutz <stepan.rutz@gmx.de>
2024-11-18 14:47:44 +0100

    box open/close

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
-import { ReactNode } from "react"
+import { ReactNode, useState } from "react"
 
 export function App() {
+    console.log("rendering app")
     return (
         <div className="bg-gray-200 w-full h-full flex flex-col gap-4 items-center">
             <Box>
@@ -16,15 +17,26 @@ export function App() {
 }
 
 function BigText(props: { text: string }) {
+    console.log("rendering bigtext: text=" + props.text)
     return (
         <div className="text-4xl">{props.text}</div>
     )
 }
 
 function Box(props: { children: ReactNode }) {
+    const [ open, setOpen ] = useState(true)
+    console.log("rendering box: open=" + open)
+    const handleClick = () => {
+        setOpen(!open)
+    }
+    const cn = "motion-preset-pop bg-white border border-gray-300 shadow-xl rounded-lg m-2 p-4"
+        + " flex flex-col gap-2 w-[300px]"
     return (
-        <div className="motion-preset-pop bg-white border border-gray-300 shadow-xl rounded-lg m-2 p-4">
-            {props.children}
+        <div className={cn} >
+            <div onClick={handleClick} className="select-none cursor-pointer self-end">X</div>
+            <div className={open ? "flex flex-col" : "hidden"}>
+                {props.children}
+            </div>
         </div>
     )
 }
