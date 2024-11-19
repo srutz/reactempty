c8aa35882f91a1d470e0daa66145e429b1962e58
sr <stepan.rutz@gmx.de>
2024-11-19 08:57:46 +0100

    more

--- src/App.tsx
+++ src/App.tsx
@@ -1,40 +1,46 @@
-import { ReactNode, useState } from "react"
+import { ReactNode, useEffect, useState } from "react"
 
 export function App() {
-    console.log("rendering app")
+    const [key, setKey] = useState(1)
+
     return (
         <div className="bg-gray-200 w-full h-full flex flex-col gap-4 items-center">
-            <Box>
-                <BigText text="hello"></BigText>
-                <BigText text="again"></BigText>
-                <div>Die Woche schneit es wahrscheinlich</div>
+            <Box key={key}>
+                <Logo title="Willkommen"></Logo>
             </Box>
-            <Box>
-                <BigText text="bonjour"></BigText>
-            </Box>
+            <button onClick={() => setKey(key + 1)}>Restart Animation</button>
         </div>
     )
 }
 
-function BigText(props: { text: string }) {
-    console.log("rendering bigtext: text=" + props.text)
-    return (
-        <div className="text-4xl">{props.text}</div>
-    )
-}
 
 function Box(props: { children: ReactNode }) {
-    const [ open, setOpen ] = useState(true)
-    console.log("rendering box: open=" + open)
-    const handleClick = () => {
-        setOpen(!open)
-    }
-    const cn = "motion-preset-pop bg-white border border-gray-300 shadow-xl rounded-lg m-2 p-4"
-        + " flex flex-col w-[300px]"
+    const cn = "motion-preset-pop bg-white border border-gray-300 shadow-xl rounded-xl m-2 p-4"
+        + " flex flex-col"
     return (
-        <div className={cn} >
-            <div onClick={handleClick} className="select-none cursor-pointer self-end">X</div>
-            {open && props.children}
+        <div className={cn} >{props.children}
         </div>
+    )
+}
+
+
+function Logo({ title }: { title: string }) {
+    const chars = title.split("")
+    return (
+        <div className="flex flex-row gap-[-3px]">
+            {chars.map((c, i) => (
+                <Char key={i} delay={1_000 + i * 200} char={c} />
+            ))}
+        </div>
+    )
+}
+
+function Char({ char, delay }: { delay: number, char: string }) {
+    const [ classes, setClasses ] = useState("motion-paused")
+    useEffect(() => {
+        setTimeout(() => setClasses("motion-running"), delay)
+    })
+    return (
+        <div className={"text-[140px] uppercase font-bold motion-preset-shrink " + classes}>{char}</div>
     )
 }
