013edbcef691ffa3464326fdb6ce73b8a24f595a
stepan.rutz <stepan.rutz@gmx.de>
2024-11-19 16:06:58 +0100

    declartive form

--- src/App.tsx
+++ src/App.tsx
@@ -7,32 +7,53 @@ export function App() {
     )
 }
 
-import { useState } from 'react'
+import { ChangeEvent, FormEvent, Fragment, HTMLInputTypeAttribute, useState } from 'react'
+
+interface FormType {
+    name: string,
+    firstName: string,
+    email: string,
+    country: string
+}
+type IdType = keyof FormType
+
+type FormSpec = { id: IdType, type: HTMLInputTypeAttribute, label: string}
+const formSpec: FormSpec[] = [
+    { id: "name", type: "text", label: "Name" },
+    { id: "firstName", type: "text", label: "Vorname" },
+    { id: "email", type: "email", label: "Nachname" },
+    { id: "country", type: "text", label: "Land" },
+]
+
 
 export function MyForm() {
-    const [name, setName] = useState('')
-    const [firstName, setFirstName] = useState('')
-    const [email, setEmail] = useState('')
+    const [ form, setForm ] = useState<FormType>({
+        name: "", firstName: "", email: "", country: ""
+    })
 
-    const handleSubmit = (e: React.FormEvent) => {
+    const handleSubmit = (e: FormEvent) => {
         e.preventDefault()
-        console.log({ name, email })
+        console.log("form object:", form)
     }
-    console.log("rendering: ", name, email)
-
     let error = ""
-    if (name.length <= 4) {
+    if (form.name.length <= 4) {
         error = "Name zu kurz"
     }
+    const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
+        // shallow copy
+        const newForm = { ...form, [e.target.id]: e.target.value }
+        setForm(newForm)
+    }
+    console.log(form)
 
     return (
         <form onSubmit={handleSubmit} className="p-4 m-4 w-64 flex flex-col gap-2 bg-white rounded-lg shadow-xl">
-            <label htmlFor="id1">Name:</label>
-            <input id="id1" type="text" value={name} onChange={(e) => setName(e.target.value)} />
-            <label htmlFor="id2">Vorname:</label>
-            <input id="id2" type="text" value={firstName} onChange={(e) => setFirstName(e.target.value)} />
-            <label htmlFor="id3">Email:</label>
-            <input id="id3" type="email" value={email} onChange={(e) => setEmail(e.target.value)} />
+            {formSpec.map(s => (
+                <Fragment key={s.id}>
+                    <label htmlFor={s.id}>{s.label}:</label>
+                    <input id={s.id} type={s.type} value={form[s.id]} onChange={handleChange} />
+                </Fragment>
+            ))}
             <button type="submit">Submit</button>
             <div className="text-red-700 font-bold">{error}</div>
         </form>
