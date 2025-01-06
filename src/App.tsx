df9b2ecfbfc6cf42f67720ae5f12bf8951877e41
stepan.rutz <stepan.rutz@gmx.de>
2025-01-06 13:16:35 +0100

    greetings

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,16 @@
 
 export function App() {
+    const leute = [ "Frank", "Hans", "Rudi", "Gisela" ]
     return (
-        <div className="text-3xl">Hello</div>
+        <div className="text-3xl">
+            {leute.map((l) => (<Greeting message={l} />))}
+        </div>
     )
+}
+
+function Greeting(props : { message: string}) {
+    const v =  <div>Ich grüße dich {
+        props.message == "Frank" ? "NICHT" : props.message}</div>
+    return v
 }
 
