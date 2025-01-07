3e066c8eb2033996d1ab796a42b3d908ff9db642
stepan.rutz <stepan.rutz@gmx.de>
2025-01-07 14:09:41 +0100

    load quote

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,28 @@
+import { useEffect, useState } from "react"
+
+type Quote = { id: number, quote: string, author: string }
 
 export function App() {
+    const [quote, setQuote] = useState<Quote>()
+    useEffect(() => {
+        const dummy = async () => {
+            const r = await fetch("https://dummyjson.com/quotes/" + rand(30))
+            const data = await r.json() as Quote
+            setQuote(data)
+        }
+        dummy()
+    }, [])
     return (
-        <div className="text-3xl">Hello</div>
+        <div className="shadow-lg rounded-lg border border-gray-300 
+                bg-white p-4 m-4">
+            {quote?.quote}
+            <div className="text-right text-gray-700 text-sm">
+                {quote?.author}</div>
+        </div>
     )
+}
+
+function rand(limit: number) {
+    return Math.floor(Math.random() * limit)
 }
 
