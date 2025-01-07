cea1a9810e2ac20cbd945ae07b685ba175a77b41
stepan.rutz <stepan.rutz@gmx.de>
2025-01-07 13:39:53 +0100

    Hook useWindowSize

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,27 @@ import { useEffect, useState } from "react"
 export function App() {
     return (
         <div className="text-3xl">
+            <Greeting></Greeting>
             <WindowSize></WindowSize>
         </div>
     )
 }
 
+export function Greeting() {
+    const size = useWindowSize()
+    console.log("render greeting", size)
+    if (size.width < 500) {
+        return <div>Schmal heute</div>
+    }
+    return <div>Wir haben Platz</div>
+}
+
 export function WindowSize() {
+    const size = useWindowSize()
+    return (<div>{size.width} x {size.height}</div>)
+}
+
+export function useWindowSize() {
     const [size, setSize] = useState({
         width: window.innerWidth,
         height: window.innerHeight
@@ -24,7 +39,5 @@ export function WindowSize() {
             window.removeEventListener("resize", l)
         }
     }, [])
-
-    return (<div>{size.width} x {size.height}</div>)
+    return size
 }
-
