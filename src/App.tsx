d95b09133e65003758c765da033e6647e3d42172
stepan.rutz <stepan.rutz@gmx.de>
2025-01-07 13:24:26 +0100

    hook

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,30 @@
+import { useEffect, useState } from "react"
 
 export function App() {
     return (
-        <div className="text-3xl">Hello</div>
+        <div className="text-3xl">
+            <WindowSize></WindowSize>
+        </div>
     )
+}
+
+export function WindowSize() {
+    const [size, setSize] = useState({
+        width: window.innerWidth,
+        height: window.innerHeight
+    })
+    useEffect(() => {
+        const l = () => {
+            setSize({ 
+                width: window.innerWidth,
+                height: window.innerHeight })
+        }
+        window.addEventListener("resize", l)
+        return () => {
+            window.removeEventListener("resize", l)
+        }
+    }, [])
+
+    return (<div>{size.width} x {size.height}</div>)
 }
 
