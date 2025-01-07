b9ef01e28873fc76138f00c0139afd597671cb24
stepan.rutz <stepan.rutz@gmx.de>
2025-01-07 14:18:01 +0100

    load quote

--- src/App.tsx
+++ src/App.tsx
@@ -3,15 +3,7 @@ import { useEffect, useState } from "react"
 type Quote = { id: number, quote: string, author: string }
 
 export function App() {
-    const [quote, setQuote] = useState<Quote>()
-    useEffect(() => {
-        const dummy = async () => {
-            const r = await fetch("https://dummyjson.com/quotes/" + rand(30))
-            const data = await r.json() as Quote
-            setQuote(data)
-        }
-        dummy()
-    }, [])
+    const quote = useQuote(rand(20))
     return (
         <div className="shadow-lg rounded-lg border border-gray-300 
                 bg-white p-4 m-4">
@@ -20,6 +12,18 @@ export function App() {
                 {quote?.author}</div>
         </div>
     )
+}
+
+export function useQuote(id: number) {
+    const [quote, setQuote] = useState<Quote>()
+    useEffect(() => {
+        (async () => {
+            const r = await fetch("https://dummyjson.com/quotes/" + id)
+            const data = await r.json() as Quote
+            setQuote(data)
+        })()
+    }, [])
+    return quote
 }
 
 function rand(limit: number) {
