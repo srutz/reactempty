683e521a62e8f8a7bcd30e87fa53d254b5aaa7b1
stepan.rutz <stepan.rutz@gmx.de>
2025-01-07 11:48:01 +0100

    styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,21 @@
+import { FC } from "react"
 
 export function App() {
+    const toggle = () => {
+    }
     return (
-        <div className="text-3xl">Hello</div>
+        <div className="p-4">
+            <button onClick={toggle} 
+                className="bg-blue-500 text-white py-2 px-4">Toggle</button>
+            <Lorem></Lorem>
+        </div>
     )
+}
+
+//export function Lorem() {
+const Lorem: FC = () => {
+    return (<div>
+        Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. 
+    </div>)
 }
 
