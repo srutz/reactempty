174b7c3731fda95b4a3480ce78712920e436340c
stepan.rutz <stepan.rutz@gmx.de>
2025-01-07 12:12:15 +0100

    styles

--- src/App.tsx
+++ src/App.tsx
@@ -9,20 +9,20 @@ export function App() {
         <div className="p-4">
             <button onClick={toggle} 
                 className="bg-blue-500 text-white py-2 px-4">Toggle</button>
-            <OpacityPanel visible={visible} >
-                Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua.
-            </OpacityPanel>
+            <OpacityPanel visible={visible} content={(
+                <div className="text-xs">Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua"</div>
+            )}></OpacityPanel>
         </div>
     )
 }
 
-const OpacityPanel: FC<{ visible?: boolean, children?: ReactNode}> = (props) => {
-    const { visible, children } = props
+const OpacityPanel: FC<{ visible?: boolean, content?: ReactNode}> = (props) => {
+    const { visible, content } = props
     const styles: CSSProperties = {
         opacity: visible ? 1 : 0,
         transition: "all 500ms",
         animationDelay: "1500ms",
     } 
-    return (<div style={styles}>{children}</div>)
+    return (<div style={styles}>{content}</div>)
 }
 
