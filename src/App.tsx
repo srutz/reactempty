78d31a04598c0e91a9c18cc4bbb73e66c9dbc9d2
stepan.rutz <stepan.rutz@gmx.de>
2025-01-13 09:54:36 +0100

    correct counter

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,23 @@
 import { useEffect, useState } from "react"
 
-let global = 1
-
 export function App() {
-
     const [ counter, setCounter ] = useState(1)
-    console.log("rerender", global++)
-
+    console.log("rerender", counter)
     useEffect(() => {
-        setInterval(() => { 
+        console.log("run effect ", counter)
+        // on Mount install timer
+        const i = setInterval(() => { 
             setCounter(counter + 1)
             console.log("interval called")
         }, 1_000)
-    }, [])
-
+        return () => {
+            clearInterval(i)  // cleanup
+        }
+    }, [ counter ])
     const handleClick = () => {
         console.log("clicked me")
         setCounter(counter + 1)
     }
-
     return (
         <button onClick={handleClick}>Button {counter}</button>
     )
