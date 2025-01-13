2554288c0f41f1ba69538ab9ded6f9738c29a723
stepan.rutz <stepan.rutz@gmx.de>
2025-01-13 09:22:55 +0100

    countertimer

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,26 @@
+import { useEffect, useState } from "react"
+
+let global = 1
 
 export function App() {
+
+    const [ counter, setCounter ] = useState(1)
+    console.log("rerender", global++)
+
+    useEffect(() => {
+        setInterval(() => { 
+            setCounter(counter + 1)
+            console.log("interval called")
+        }, 1_000)
+    }, [])
+
+    const handleClick = () => {
+        console.log("clicked me")
+        setCounter(counter + 1)
+    }
+
     return (
-        <div className="text-3xl">Hello</div>
+        <button onClick={handleClick}>Button {counter}</button>
     )
 }
 
