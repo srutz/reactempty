44bd9e47aac28e661a9d4d6e7127b44309dc3d37
stepan.rutz <stepan.rutz@gmx.de>
2025-01-14 12:03:55 +0100

    signup form mit button

--- src/SignupForm.tsx
+++ src/SignupForm.tsx
@@ -4,11 +4,12 @@ import { TextInput } from "./TextInput"
 export type SignupFormType = {
     firstname: string,
     lastname: string,
+    specialNeeds: boolean
 }
 export function SignupForm() {
     console.log("render form")
     const [ form, setForm ] = useState({
-        firstname: "", lastname: "",
+        firstname: "", lastname: "", specialNeeds: false
     } as SignupFormType)
 
     const handleSubmit = (event: FormEvent<HTMLFormElement>) => {
@@ -18,20 +19,27 @@ export function SignupForm() {
     return (
         <form className="flex flex-col" onSubmit={handleSubmit}>
 
-            <TextInput label="Firstname"  id="x"
+            <TextInput label="Firstname" id="x"
                 placeholder="Firstname" value={form.firstname}
                 onChange={(e) => { setForm({ ...form, firstname: e.target.value })}}
-                infoMessage={form.firstname.length <= 20 && `Noch ${20 - form.firstname.length} Zeichen`}
                 errorMessage={form.lastname.length > 20 && `Echt langer Name`}>
             </TextInput>
-            <Label htmlFor="lastname">Lastname</Label>
-            <input id="lastname" placeholder="Lastname" 
-                value={form.lastname}
-                onChange={(e) => { 
-                    setForm({ ...form, lastname: e.target.value })
-                 }}
-                ></input>
-            <button type="submit">Submit</button>
+            <TextInput label="lastname" id="y"
+                placeholder="Lastname" value={form.lastname}
+                onChange={(e) => { setForm({ ...form, lastname: e.target.value })}}>
+            </TextInput>
+            <div className="flex gap-4">
+                <label id="z">Ich habe die AGB gelesen</label>
+                <input type="checkbox" checked={form.specialNeeds} onChange={(e) => {
+                    console.log(e.target.value)
+                    setForm({ ...form, specialNeeds: e.target.checked})
+                }}></input>
+            </div>
+            <button disabled={!form.specialNeeds} 
+    className="self-center disabled:text-gray-300 disabled:bg-gray-400 
+            bg-blue-500 hover:bg-blue-600 
+            rounded px-4 py-1 text-white"
+                type="submit">Submit</button>
         </form>
     )
 }
