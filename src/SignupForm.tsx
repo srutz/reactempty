14b2cac5a07de3d2175c23bdeee579e0a203f536
stepan.rutz <stepan.rutz@gmx.de>
2025-01-14 11:14:37 +0100

    error and success feedback

--- src/SignupForm.tsx
+++ src/SignupForm.tsx
@@ -5,30 +5,34 @@ export type SignupFormType = {
     lastname: string,
 }
 export function SignupForm() {
+    console.log("render form")
     const [ form, setForm ] = useState({
         firstname: "", lastname: "",
     } as SignupFormType)
 
     const handleSubmit = (event: FormEvent<HTMLFormElement>) => {
         event.preventDefault()
-        console.log("submitting form", form)
+        console.log("send form", form)
     }
     return (
         <form className="flex flex-col" onSubmit={handleSubmit}>
             <Label htmlFor="firstname">Firstname</Label>
             <input id="firstname" placeholder="Firstname" 
                 value={form.firstname}
-                onChange={(e) => { setFirstname(e.target.value) }}
+                onChange={(e) => { 
+                    setForm({ ...form, firstname: e.target.value })
+                 }}
                 ></input>
             {form.firstname.length > 20 
                 ? ( <div className="text-red-600 text-xs">Echt langer Name</div>)
-                : ( <div className="text-gray-400 text-xs">
-                    Noch {20 - form.firstname.length} Zeichen</div>)
+                : ( <div className="text-gray-400 text-xs">Noch {20 - form.firstname.length} Zeichen</div>)
             }
             <Label htmlFor="lastname">Lastname</Label>
             <input id="lastname" placeholder="Lastname" 
                 value={form.lastname}
-                onChange={(e) => { setLastname(e.target.value) }}
+                onChange={(e) => { 
+                    setForm({ ...form, lastname: e.target.value })
+                 }}
                 ></input>
             <button type="submit">Submit</button>
         </form>
