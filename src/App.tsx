be9aecd7e32589e2dc696eaec97f1b9e105ebd02
stepan.rutz <stepan.rutz@gmx.de>
2025-01-15 13:17:07 +0100

    box2 branch

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,22 @@
+import { ReactNode } from "react";
 
 export function App() {
     return (
-        <div className="text-3xl">Hello</div>
+        <div className="text-3xl">
+            <Box extra="abc"></Box>
+            <Box extra="defg"></Box>
+            <Box></Box>
+        </div>
+    )}
+
+
+function Box(props : { extra?: string}) {
+    return (
+        <div className="m-4 p-4 rounded-lg shadow-xl
+            border border-gray-300
+            bg-white flex flex-col motion-preset-slide">
+                {props.extra}
+        </div>
+
     )
 }
-
