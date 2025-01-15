fa47d08c78c121f655b6be42892cc5aad83a361e
stepan.rutz <stepan.rutz@gmx.de>
2025-01-15 15:44:59 +0100

    window size listener

--- src/App.tsx
+++ src/App.tsx
@@ -11,22 +11,28 @@ export function App() {
 }
 
 function Box(props: { title: string, children?: ReactNode }) {
-    const [open, setOpen] = useState(false)
+    const [size, setSize] = useState({ 
+        width: window.innerWidth,
+        height: window.innerHeight})
     useEffect(() => {
-
+        const l = () => {
+            setSize({ width: window.innerWidth, height: window.innerHeight })
+        }
+        window.addEventListener("resize", l)
+        return () => { window.removeEventListener("resize", l) }
     }, [])
+
+    const [open, setOpen] = useState(false)
     const handleClick = () => setOpen(!open)
-    console.log("Box opened")
     return (
         <div className="m-4 py-2 px-4 rounded-lg shadow-xl
             border border-gray-300 cursor-pointer select-none
             bg-white flex flex-col">
             <div className="justify-between flex border-b border-gray-500 pb-2 mb-4">
-                <div>{props.title}</div>
+                <div>{props.title} {size.width} x {size.height}</div>
                 <button onClick={handleClick}
                   className="text-sm">{open ? "▼" : "◄"}</button>
             </div>
-
             {open && <div className="motion-preset-pop">{props.children}</div>}
         </div>
     )
