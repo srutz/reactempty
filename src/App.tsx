6b5385caa9fffecadbd552ef098c092f8134239b
stepan.rutz <stepan.rutz@gmx.de>
2025-01-16 10:37:13 +0100

    loadingproduct

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,28 @@
+import { useEffect, useState } from "react"
+
+export type Product = {
+    id: number,
+    title: string,
+    description: string,
+    rating: number,
+    price: number,
+    thumbnail: string
+}
 
 export function App() {
+    const [ product, setProduct] = useState<Product>()
+    useEffect(() => {
+        fetch("https://dummyjson.com/product/17").
+        then((result) => {
+            return result.json()
+        }).
+        then((data) => {
+            setProduct(data)
+        })
+    }, [])
+    if (!product) { return <div></div> }
     return (
-        <div className="text-3xl">Hello</div>
+        <pre className="">{JSON.stringify(product,null,4)}</pre>
     )
 }
 
