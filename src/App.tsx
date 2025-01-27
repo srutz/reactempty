e2539dec9f137c5d857aa98051128c37c9041335
stepan.rutz <stepan.rutz@gmx.de>
2025-01-27 14:04:23 +0100

    working box

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
-import { ReactNode } from "react"
-import { MdStar } from "react-icons/md"
+import { ReactNode, useState } from "react"
+import { MdStar, MdTableBar } from "react-icons/md"
 
 export function App() {
     return (
@@ -17,17 +17,28 @@ function Greetings() { return <div>Hallo zusammen</div> }
 
 type BoxProps = { heading: string, star?: boolean, children: ReactNode }
 
-
 function Box({ heading, star, children }: BoxProps) {
+    const [ open, setOpen ] = useState<boolean>(false)
+    console.log("render Box", heading)
+    function handleClick() {
+        setOpen(!open)
+    } 
     return (
         <div className="bg-gray-300 shadow-xl rounded-lg p-4 m-4 flex flex-col gap-2">
-            <div className="text-sm text-gray-600 flex justify-between items-center border-b border-gray-400 pb-2">
-                <span>{heading}</span>
-                {star && <MdStar />}
+            <div className="text-sm text-gray-600 flex justify-between items-center pb-2">
+                <button onClick={handleClick}>
+                    {open ? "▾" : "▸"}
+                </button>
+                <div className="flex items-center gap-2">
+                    <span>{heading}</span>
+                    {star && <MdStar />}
+                </div>
             </div>
-            <div className="mt-2">
-                {children}
-            </div>
+            {open && (
+                <div className="mt-2">
+                    {children}
+                </div>
+            )}
         </div>
     )
 }
