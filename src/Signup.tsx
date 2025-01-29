2baeeda0cb753dd96611ba0f70912259ec9eda5c
stepan.rutz <stepan.rutz@gmx.de>
2025-01-29 11:30:59 +0100

    basic form

--- src/Signup.tsx
+++ src/Signup.tsx
@@ -1,8 +1,10 @@
-import { Fragment, ReactNode, useState } from "react"
+import { FormEvent, Fragment, HTMLInputTypeAttribute, ReactNode, useState } from "react"
 
 type SignupForm = {
     firstname: string
     lastname: string
+    street: string
+    city: string
     email: string
 }
 
@@ -10,37 +12,66 @@ export function Box({children }: { children: ReactNode}) {
     return (<div className="p-4 m-4 shadow-xl bg-white">{children}</div>)
 }
 
+export function ErrorText({children}: { children: ReactNode}) {
+    return (<div className="text-sm text-red-600">{children}</div>)
+}
+
 export function Signup() {
     const [form, setForm ] = useState<SignupForm>({
         firstname: "",
         lastname: "",
         email: "",
+        street: "",
+        city: "",
     })
+    const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
+        e.preventDefault()
+        console.log(form)
+    }
     return (
     <Box>
-        <form className="flex flex-col gap-2">
+        <form onSubmit={(e) => handleSubmit(e)} className="flex flex-col gap-2">
             <TextInput label="Vorname"
                 value={form.firstname} 
+                errorMsg={
+                    form.firstname.length > 0 && form.firstname.length < 5 && (
+                        <ErrorText>Vorname zu kurz</ErrorText>
+                    )
+                }
                 onChange={(v) => setForm({...form, firstname: v})}></TextInput>
             <TextInput label="Nachname"
                 value={form.lastname} 
                 onChange={(v) => setForm({...form, lastname: v})}></TextInput>
+            <TextInput label="Straße"
+                value={form.street} 
+                onChange={(v) => setForm({...form, street: v})}></TextInput>
+            <TextInput label="Stadt"
+                value={form.city} 
+                onChange={(v) => setForm({...form, city: v})}></TextInput>
+            <TextInput label="Email"
+                value={form.email} 
+                type="email"
+                onChange={(v) => setForm({...form, email: v})}></TextInput>
+            <button className="self-center" type="submit">Submit</button>
         </form>
     </Box>
     )
 }
 
 type TextInputProps = {
-    label: string
-    value: string
+    label: string, value: string, type?: HTMLInputTypeAttribute,
+    errorMsg?: ReactNode,
     onChange: (value: string) => void
 }
 
-export function TextInput({ label, value, onChange }: TextInputProps) {
+export function TextInput({ label, value, type, onChange, errorMsg }: TextInputProps) {
     return (
-        <div className="flex gap-2 items-baseline">
+        <div className="grid grid-cols-[auto,1fr] gap-x-2 items-baseline mb-2">
             <label className="w-32" htmlFor={label}>{label}</label>
-            <input id={label} value={value} onChange={(e) => onChange(e.target.value)}/>
+            <input type={type}
+                id={label} value={value} onChange={(e) => onChange(e.target.value)}/>
+            <div></div>
+            {errorMsg}
         </div>
     )
 }
