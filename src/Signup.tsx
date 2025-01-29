a70d1bc9efad07bdec400962d7f17e538dafddb9
stepan.rutz <stepan.rutz@gmx.de>
2025-01-29 11:06:05 +0100

    basic form

--- src/Signup.tsx
+++ src/Signup.tsx
@@ -1,4 +1,4 @@
-import { ReactNode, useState } from "react"
+import { Fragment, ReactNode, useState } from "react"
 
 type SignupForm = {
     firstname: string
@@ -17,17 +17,30 @@ export function Signup() {
         email: "",
     })
     return (
-        <Box>
-            <form>
-                <label>Vorname</label>
-                <input value={form.firstname} onChange={
-                    (e) => setForm({ ... form, firstname: e.target.value })
-                }></input>
-                {form.firstname == "Fritz" && (
-                    <div>Blöder Name</div>
-                )}
-            </form>
-        </Box>
+    <Box>
+        <form className="flex flex-col gap-2">
+            <TextInput label="Vorname"
+                value={form.firstname} 
+                onChange={(v) => setForm({...form, firstname: v})}></TextInput>
+            <TextInput label="Nachname"
+                value={form.lastname} 
+                onChange={(v) => setForm({...form, lastname: v})}></TextInput>
+        </form>
+    </Box>
     )
+}
 
+type TextInputProps = {
+    label: string
+    value: string
+    onChange: (value: string) => void
+}
+
+export function TextInput({ label, value, onChange }: TextInputProps) {
+    return (
+        <div className="flex gap-2 items-baseline">
+            <label className="w-32" htmlFor={label}>{label}</label>
+            <input id={label} value={value} onChange={(e) => onChange(e.target.value)}/>
+        </div>
+    )
 }
